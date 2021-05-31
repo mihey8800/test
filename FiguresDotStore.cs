0f671198097b04b2876a507eb21d74aeb87471e4
mihey8800 <mihey8800@gmail.com>
2021-05-31 20:29:37 +0700

    test solution

--- FiguresDotStore.cs
+++ FiguresDotStore.cs
@@ -1,5 +1,7 @@
 using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Diagnostics.CodeAnalysis;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
@@ -18,7 +20,7 @@ namespace FiguresDotStore.Controllers
 		// корректно сконфигурированный и готовый к использованию клиент Редиса
 		private static IRedisClient RedisClient { get; }
 
-		public static bool CheckIfAvailable(string type, int count)
+        public static bool CheckIfAvailable(string type, int count)
 		{
 			return RedisClient.Get(type) >= count;
 		}
@@ -31,6 +33,8 @@ namespace FiguresDotStore.Controllers
 		}
 	}
 
+
+	
 	public class Position
 	{
 		public string Type { get; set; }
@@ -44,18 +48,24 @@ namespace FiguresDotStore.Controllers
 
 	public class Cart
 	{
+		//пердполагаем, что в пустой корзине смысла нет
+		[NotNull]
+        [Required]
 		public List<Position> Positions { get; set; }
 	}
 
 	public class Order
 	{
+		[NotNull]
+		[Required]
 		public List<Figure> Positions { get; set; }
 
 		public decimal GetTotal() =>
 			Positions.Select(p => p switch
 			{
-				Triangle => (decimal)p.GetArea() * 1.2m,
-				Circle => (decimal)p.GetArea() * 0.9m
+                Triangle _ => (decimal)p.GetArea() * 1.2m,
+                Circle _ => (decimal)p.GetArea() * 0.9m,
+                _ => throw new ArgumentException("Invalid type")
 			})
 				.Sum();
 	}
@@ -96,8 +106,8 @@ namespace FiguresDotStore.Controllers
 		{
 			if (SideA < 0)
 				throw new InvalidOperationException("Square restrictions not met");
-
-			if (SideA != SideB)
+            //тут не хватало tolerance. вроде мы говорим о геометрии и сравнении фигур, скорей всего это важно. 
+			if (Math.Abs(SideA - SideB) > 5)
 				throw new InvalidOperationException("Square restrictions not met");
 		}
 
@@ -127,8 +137,9 @@ namespace FiguresDotStore.Controllers
 	{
 		private readonly ILogger<FiguresController> _logger;
 		private readonly IOrderStorage _orderStorage;
+        private readonly object _lockobject = new object();
 
-		public FiguresController(ILogger<FiguresController> logger, IOrderStorage orderStorage)
+        public FiguresController(ILogger<FiguresController> logger, IOrderStorage orderStorage)
 		{
 			_logger = logger;
 			_orderStorage = orderStorage;
@@ -137,41 +148,72 @@ namespace FiguresDotStore.Controllers
 		// хотим оформить заказ и получить в ответе его стоимость
 		[HttpPost]
 		public async Task<ActionResult> Order(Cart cart)
-		{
-			foreach (var position in cart.Positions)
-			{
-				if (!FiguresStorage.CheckIfAvailable(position.Type, position.Count))
-				{
-					return new BadRequestResult();
-				}
+        {
+            //валидируем, чтобы не прислали нули. т.к. поставили dbannotation, должно работать.
+            if (!TryValidateModel(cart)) return BadRequest();
+            //проверяем недоступные позиции
+            var notAvailablePositions =
+                cart.Positions.Where(position => !FiguresStorage.CheckIfAvailable(position.Type, position.Count)).ToList();
+			if (notAvailablePositions.Count > 0)
+            {
+                //предполагаем, что мы вообще не создаем ордер, если есть недоступные позиции, есть конечно вариант, что можно создавать не только из доступных, но без тз - не ясно.
+                //возвращаем заодно клиенту, то - что недоступно, может в интерфейсе отобразят
+				return new NotFoundObjectResult(notAvailablePositions);
 			}
-
+            
 			var order = new Order
 			{
-				Positions = cart.Positions.Select(p =>
+				//переписал на test type pattern. вроде версия .net 5 c# 9, но не работало
+                Positions = cart.Positions.Select(p =>
 				{
-					Figure figure = p.Type switch
-					{
-						"Circle" => new Circle(),
-						"Triangle" => new Triangle(),
-						"Square" => new Square()
-					};
-					figure.SideA = p.SideA;
-					figure.SideB = p.SideB;
-					figure.SideC = p.SideC;
-					figure.Validate();
-					return figure;
-				}).ToList()
+                    var figure = p.Type switch
+                    {
+                        "Circle" =>(Figure) new Circle(),
+                        "Triangle" => new Triangle(),
+                        "Square" => new Square(),
+                        _ => null
+                    };
+
+                    if (figure == null) return null;
+                    figure.SideA = p.SideA;
+                    figure.SideB = p.SideB;
+                    figure.SideC = p.SideC;
+                    try
+                    {
+						//т.к. validate возвращает excepшны то надо чекнуть и вернуть null, если не вышло.
+                        figure.Validate();
+                    }
+                    catch
+                    {
+                        return null;
+                    }
+                    
+                    return figure;
+
+                }).ToList()
 			};
 
 			foreach (var position in cart.Positions)
 			{
-				FiguresStorage.Reserve(position.Type, position.Count);
+				//это делаем для мультитреда в синглинстанс. для мультиинстанса нужен Distributed lock,
+				//но писать его надо в репозитории. Раз уж у нас корректно сконфигурированный и готовый к использованию клиент Redis'a,
+				//предполагаем, что оно там сделано и мультиинстансы не убьют параллельно идущие данные
+				// я про вот это https://github.com/kidfashion/redlock-cs. Ожидаю, что в redis.set где-то внутри оно используется.
+				lock (_lockobject)
+                {
+					FiguresStorage.Reserve(position.Type, position.Count);
+				}
+				
 			}
 
-			var result = _orderStorage.Save(order);
+            //к этому месту есть вопросы, реализация работы с данными скрыта сейчас, так что пердполагаем, что везде добавлены transacrion.rollback ну или что-то подходящее, 
+            var result = await _orderStorage.Save(order);
+            // предполагаем, что сумма должна быть не нулевой после сохранения, если она 0, то возвращаем 500 ошибку. 
+            if (result > 0) return new OkObjectResult(result);
+			return new StatusCodeResult(500);
 
-			return new OkObjectResult(result.Result);
+
+
 		}
 	}
 }
